a07b8ee459111ddfb514caac00e12000b62d1c1b
mojan <hojat72elect@gmail.com>
2022-04-12 00:45:53 -0400

    Implemented ListView for main page

--- app/src/main/java/ca/sudbury/hojat/zahra/MainActivity.kt
+++ app/src/main/java/ca/sudbury/hojat/zahra/MainActivity.kt
@@ -1,11 +1,39 @@
 package ca.sudbury.hojat.zahra
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import ca.sudbury.hojat.zahra.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // All GUI elements in this repo
+        val GUI_Elements =
+            arrayListOf("GridLayout", "NavigationDrawer", "AppCompatEditText", "Button")
+
+        // ArrayAdapter for list
+        val arrayAdapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, GUI_Elements)
+
+        binding.listViewMainPage.let {
+            it.adapter = arrayAdapter
+            it.setOnItemClickListener { _, _, position, _ ->
+
+                Toast.makeText(this, "$position", Toast.LENGTH_SHORT).show()
+//                when (position) {
+//                    0 -> Toast.makeText(this, "0", Toast.LENGTH_SHORT).show()
+//                }
+            }
+        }
+
+
     }
 }
